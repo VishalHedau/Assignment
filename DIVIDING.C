440e3c7b9a767446126414e86973894d7900c5c5
VishalHedau <118975915+VishalHedau@users.noreply.github.com>
2022-12-01 21:11:17 -0800

    dividingby7.c

---
+++ DIVIDING.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int x,y,temp,i,sum=0;
+printf("Input the first integer : ");
+scanf("%d",&x);
+printf("Inputr the second integer : ");
+scanf("%d",&y);
+if(x>y)
+{
+temp=y;
+y=x;
+x=temp;
+}
+for(i = x+1;i<y;i++)
+{
+if((i%7) == 2 || (i%7) == 3)
+{
+printf("%d\n",i);
+}
+
+}
+getch();
+}
